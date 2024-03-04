9ec56f93c8975066d81cf5168e676bbc2858e382
Restu Gede Purnama <restumelenoy83@ummi.ac.id>
2024-03-04 16:07:18 +0700

    Instance user di setUp function session service test

--- test/Repository/SessionRepositoryTest.php
+++ test/Repository/SessionRepositoryTest.php
@@ -6,16 +6,27 @@ use PDO;
 use PHPUnit\Framework\TestCase;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 
 class SessionRepositoryTest extends TestCase 
 {
+    private UserRepository $userRepository;
     private SessionRepository $sessionRepository;
 
     public function setUp(): void
     {
+        $this->userRepository = new UserRepository(Database::getConnection());
         $this->sessionRepository = new SessionRepository(Database::getConnection());
         
         $this->sessionRepository->deleteAll();
+        $this->userRepository->deleteAll();
+
+        $user = new User();
+        $user->id = "restu";
+        $user->name = "restu";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
     }
 
     public function testSaveSuccess() 
