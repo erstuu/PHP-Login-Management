5ac35584f6b7fcf4b03be1029d8435407df4eaa4
Restu Gede Purnama <restumelenoy83@ummi.ac.id>
2024-03-05 22:26:31 +0700

    Menambahkan unit test UserRepository

--- test/Repository/UserRepositoryTest.php
+++ test/Repository/UserRepositoryTest.php
@@ -6,14 +6,18 @@ use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
-
+use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
 
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     public function setUp(): void 
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
+
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -37,5 +41,22 @@ class UserRepositoryTest extends TestCase
     public function testFindByIdNotFound() {
         $user = $this->userRepository->findById("Not Found!");
         Assert::assertNull($user);
+    }
+
+    public function testUpdate() {
+        $user = new User();
+        $user->id = "Restu";
+        $user->name = "Restu";
+        $user->password = "Restu";
+
+        $this->userRepository->save($user);
+
+        $user->id = "Restu";
+
+        $result = $this->userRepository->findById($user->id);
+
+        Assert::assertEquals($user->id, $result->id);
+        Assert::assertEquals($user->name, $result->name);
+        Assert::assertEquals($user->password, $result->password);
     }
 }
